ec3fda380a2aa6477e1a81a5642ff89368895924
Yukesh Kumar <43610471+TheYk98@users.noreply.github.com>
2019-07-21 18:23:29 +0530

    Create GUVI-pos_neg.java

---
+++ GUVI-pos_neg.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n>0) 
+		 System.out.println("Positive");
+		else if(n==0)
+		  System.out.print("Zero");
+		 else
+		  System.out.println("Negative");
+	}
+}
